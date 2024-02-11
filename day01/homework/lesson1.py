05df18d8af9fdb659cd0dd4878ba9744fe5d34c1
lukaminimina <lukaminashvili8@gmail.com>
2024-02-11 17:42:24 +0400

    second commit

---
+++ day01/homework/lesson1.py
@@ -0,0 +1,5 @@
+name = "my name is luka"
+surename = "my surename is minashvili"
+age = "am 10 years old"
+story = "i wanted to be come programer and now im learining in goal orientid academy"
+print(name + " " + surename + " " + age + " " + story)
